518175a306ab088407a412bed5edd27ce4f6507d
Pedro Silva <DarthPedro@gmail.com>
2025-11-02 00:49:36 -0700

    Moved Lexer magic strings to new Constants class.

---
+++ src/d20Tek.DiceNotation/Parser/Constants.cs
@@ -0,0 +1,16 @@
+﻿namespace d20Tek.DiceNotation.Parser;
+
+internal static class Constants
+{
+    public const string PercentileNotation = "d%";
+    public const string D100EquivalentNotation = "d100";
+    public const string DefaultOperator = "x";
+    public const string GroupStartOperator = "(";
+    public const string GroupEndOperator = ")";
+    public const string DefaultNumDice = "1";
+    public const string DiceOperator = "d";
+    public const string FudgeDiceOperator = "f";
+
+    public static readonly HashSet<string> Operators = ["d", "f", "k", "p", "l", "!", "/", "x", "*", "-", "+"];
+    public static Token DefaultDiceNumberToken = new(TokenType.Number, "1");
+}
--- src/d20Tek.DiceNotation/Parser/Lexer.cs
+++ src/d20Tek.DiceNotation/Parser/Lexer.cs
@@ -4,28 +4,17 @@ namespace d20Tek.DiceNotation.Parser;
 
 internal sealed partial class Lexer
 {
-    private static Token _defaultDiceNumberToken = new(TokenType.Number, "1");
-
     public IEnumerable<Token> Tokenize(string expression)
     {
         ArgumentNullException.ThrowIfNullOrWhiteSpace(expression);
-        expression = CleanExpression(expression);
         Token? previous = null;
+        expression = CleanExpression(expression);
 
         foreach (Match match in TokenRegex().Matches(expression))
         {
-            string v = match.Value;
-            Token token = v switch
-            {
-                "(" => new(TokenType.GroupStart, v),
-                ")" => new(TokenType.GroupEnd, v),
-                "+" or "-" or "*" or "/" or "!" or "k" or "p" or "l" or "f" or "d"
-                    => new(TokenType.Operator, v),
-                _ when int.TryParse(v, out _) => new(TokenType.Number, v),
-                _ => new(TokenType.Identifier, v),
-            };
+            var token = MapValueToToken(match.Value);
 
-            if (HasDefaultDiceNumber(token, previous)) yield return _defaultDiceNumberToken;
+            if (HasDefaultDiceNumber(token, previous)) yield return Constants.DefaultDiceNumberToken;
 
             yield return token;
             previous = token;
@@ -34,11 +23,21 @@ internal sealed partial class Lexer
         yield return new(TokenType.EndOfInput, string.Empty);
     }
 
+    private static Token MapValueToToken(string v) => v switch
+    {
+        Constants.GroupStartOperator => new(TokenType.GroupStart, v),
+        Constants.GroupEndOperator => new(TokenType.GroupEnd, v),
+        _ when Constants.Operators.Contains(v) => new(TokenType.Operator, v),
+        _ when int.TryParse(v, out _) => new(TokenType.Number, v),
+        _ => new(TokenType.Identifier, v),
+    };
+
     private static bool HasDefaultDiceNumber(Token t, Token? p) =>
-        t.Value is "d" or "f" && (p is null || p.Type is not TokenType.Number && p.Value is not ")");
+        t.Value is Constants.DiceOperator or Constants.FudgeDiceOperator && 
+            (p is null || p.Type is not TokenType.Number && p.Value is not Constants.GroupEndOperator);
 
     private static string CleanExpression(string expression) =>
-        expression.Replace("d%", "d100")
+        expression.Replace(Constants.PercentileNotation, Constants.D100EquivalentNotation)
                   .Replace("--", "+")
                   .Replace("+-", "-")
                   .Replace("-+", "-")
