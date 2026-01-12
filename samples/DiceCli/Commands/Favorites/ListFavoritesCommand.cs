09ef6a0bcbe47613a86d78526a9aa81728993a76
Pedro Silva <DarthPedro@gmail.com>
2026-01-11 16:32:27 -0800

    Implemented ListFavoritesCommand to show table of FavoriteRoll entities.

--- samples/DiceCli/Commands/Favorites/ListFavoritesCommand.cs
+++ samples/DiceCli/Commands/Favorites/ListFavoritesCommand.cs
@@ -1,6 +1,28 @@
-﻿namespace DiceCli.Commands.Favorites;
+﻿using DiceCli.Models;
+using Spectre.Console;
 
-internal class ListFavoritesCommand : Command
+namespace DiceCli.Commands.Favorites;
+
+internal class ListFavoritesCommand(IAnsiConsole console, LowDb<FavoriteRollsDocument> db) : Command
 {
-    public override int Execute(CommandContext context, CancellationToken cancellationToken) => throw new NotImplementedException();
+    private readonly IAnsiConsole _console = console;
+    private readonly LowDb<FavoriteRollsDocument> _db = db;
+
+    public override int Execute(CommandContext context, CancellationToken cancellationToken) =>
+        db.Get().ToIdentity()
+          .Iter(favoriteDoc =>
+                (favoriteDoc.Rolls.Count is 0).IfTrueOrElse(
+                    () => _console.WriteLine("No favorite rolls found. Add some favorites..."),
+                    () => DisplayRollsList(favoriteDoc.Rolls)))
+          .Map(_ => 0);
+
+    private void DisplayRollsList(IEnumerable<FavoriteRoll> rolls) =>
+        CreateFavoritesTable()
+            .Iter(t => rolls.ForEach(roll => t.AddRow(roll.Id, roll.Name, roll.Expression)))
+            .Iter(t => _console.Write(t));
+
+    private static Identity<Table> CreateFavoritesTable() =>
+        new Table().AddColumn("Identifier")
+                   .AddColumn("Display Name")
+                   .AddColumn("Notation");
 }
--- samples/DiceCli/Program.cs
+++ samples/DiceCli/Program.cs
@@ -1,8 +1,10 @@
-﻿global using D20Tek.Spectre.Console.Extensions;
+﻿global using D20Tek.LowDb;
+global using D20Tek.Spectre.Console.Extensions;
 global using D20Tek.Spectre.Console.Extensions.Commands;
 global using D20Tek.Spectre.Console.Extensions.Controls;
 global using Spectre.Console;
 global using Spectre.Console.Cli;
+global using D20Tek.Functional;
 
 using DiceCli;
 using DiceCli.Commands;
